1192219c037b9b874f3f9c4f0abb11d0c9b5fac3
Karalchuk Tema <artuom441@gmail.com>
2024-02-11 20:15:04 +0300

    feat: done

--- src/com/teachmeskills/lesson11/Main.java
+++ src/com/teachmeskills/lesson11/Main.java
@@ -1,8 +1,21 @@
+package com.teachmeskills.lesson11;
+
+import static com.teachmeskills.lesson11.utils.Example.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        String str = "5555-AbC-4231-qfB-1a2b";
+        firstTask(str);
+        secondTask(str);
+        thirdTask(str);
+        fourthTask(str);
+        fifthTask(str);
+        sixthTask(str);
+        seventhTask(str);
 
-
+        String str2 = "Creat a task for Tema";
+        minMaxWord(str2);
+        dublicate(str2);
     }
 }
--- src/com/teachmeskills/lesson11/utils/Example.java
+++ src/com/teachmeskills/lesson11/utils/Example.java
@@ -1,2 +1,92 @@
-package com.teachmeskills.lesson11.utils;public class Example {
+package com.teachmeskills.lesson11.utils;
+
+public class Example {
+
+    public static void firstTask(String str) {
+        String[] array = str.split("-");
+        str = array[0] + "-" + array[2];
+        System.out.println(str);
+    }
+
+    public static void secondTask(String str) {
+        String[] array = str.split("-");
+        str = str.replace(array[1], "***").replace(array[3], "***");
+        System.out.println(str);
+    }
+
+    public static String thirdTask(String str){
+        String[] array = str.split("-");
+        str = (array[1] +"/"+array[3]+"/"+array[4].charAt(1)+"/"+array[4].charAt(3)).toLowerCase();
+        System.out.println(str);
+        return str;
+    }
+
+    public static void fourthTask(String str){
+        StringBuilder sb = new StringBuilder();
+        String s = "Letters:".toUpperCase();
+        sb.append(s);
+        str = thirdTask(str).toUpperCase();
+        sb.append(str);
+        System.out.println(sb);
+    }
+
+    public static void fifthTask(String str){
+        if (str.toLowerCase().contains("abc")){
+            System.out.println("Последовательность 'abc' есть");
+        }else {
+            System.out.println("Последовательности 'abc' нет");
+        }
+    }
+
+    public static void sixthTask(String str){
+        if (str.startsWith("555")){
+            System.out.println("Начинается на последовательность '555' ");
+        }else {
+            System.out.println("Не начинается на последовательность '555'");
+        }
+    }
+    public static void seventhTask(String str){
+        if (str.endsWith("1a2b")){
+            System.out.println("Заканчивается на последовательность '1a2b' ");
+        }else {
+            System.out.println("Не заканчивается на последовательность '1a2b'");
+        }
+    }
+
+    public static void minMaxWord(String str){
+        System.out.println("\n"+str);
+        String[] a = str.split(" ");
+        int min = a[0].length();
+        int max = a[0].length();
+        String minStr ="",maxStr="";
+        for (int i = 0; i < a.length; i++) {
+            if (min > a[i].length()){
+                min = a[i].length();
+                minStr = a[i];
+            }else if (max <= a[i].length()){
+                max = a[i].length();
+                maxStr = a[i];
+            }
+        }
+        System.out.println("min - "+ min + " max - " + max);
+        System.out.println("minStr - "+ minStr + " maxStr - " + maxStr);
+        for (String agr:a){
+            System.out.print(agr.length() + " ");
+        }
+    }
+
+    public static void dublicate(String str){
+        System.out.println("\n" + str);
+        char[] strCharArray = str.toCharArray();
+        StringBuilder a = new StringBuilder();
+        for (char c: strCharArray){
+            if (c != ' ' & c != '-'){
+                a.append(c).append(c);
+            }else {
+                a.append(" ");
+            }
+        }
+        System.out.println(a);
+    }
+
 }
